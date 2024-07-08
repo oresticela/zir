e7f3ff7465176bef952d7d8f34a6a0a7e83f755e
Oresti Çela <75319139+oresticela@users.noreply.github.com>
2024-07-08 20:53:04 +0200

    a

--- app.py
+++ app.py
@@ -28,38 +28,13 @@ paypalrestsdk.configure({
 def index():
     return render_template('index.html')
 
-@app.route('/signupleader', methods=['POST', 'GET'])
-def signupleader():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        
-        if users.find_one({"email": email}):
-            flash('Email already exists. Please use another email.', 'error')
-            return redirect(url_for('signupleader'))
-        
-        new_user = {
-            "name": name,
-            "email": email,
-            "password": password,
-            "account": "leader",
-            "group": "0"
-        }
-        
-        users.insert_one(new_user)
-        flash('Sign-up successful! Please log in.', 'success')
-        return redirect(url_for('login'))
-
-    return render_template('signupleader.html')
-
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     # Ensure only logged-in users can access this page
     if 'user_id' not in session:
         flash('You must be logged in to view this page.', 'error')
         return redirect(url_for('login'))
-    
+
     print(session)
 
 
@@ -75,19 +50,19 @@ def home():
             todos.insert_one({str(user['group']): str(todo)})
 
         return redirect(url_for('home'))
-    
+
     user = users.find_one({"email": session['email']})
-    
+
     cursor = notes.find({str(user['group']): {"$exists": True}})
     cursor_todo = todos.find({str(user['group']): {"$exists": True}})
     cursor_todo_done = todosdone.find({str(user['group']): {"$exists": True}})
     todos_done = [document_todo_done.get(str(user['group'])) for document_todo_done in cursor_todo_done]
 
-    
+
     # Extract the notes
     group_notes = [document.get(str(user['group'])) for document in cursor]
     group_todos = [document_todo.get(str(user['group'])) for document_todo in cursor_todo]
-        
+
     user = users.find_one({"email": user['email']})
 
     your_group = groups.find_one({"code": user['group']})
@@ -97,20 +72,20 @@ def home():
 def delete_note():
     if request.method == 'POST':
         note_to_delete = request.form.get('note')
-        
+
         # Find and delete the document containing the specific note
         notes.delete_one({str(session['group']): note_to_delete})
-        
+
     return redirect(url_for('home'))
 
 @app.route('/deletetodo', methods=['POST'])
 def delete_todo():
     if request.method == 'POST':
         todo_to_delete = request.form.get('todo')
-        
+
         # Find and delete the document containing the specific note
         todos.delete_one({str(session['group']): todo_to_delete})
-        
+
     return redirect(url_for('home'))
 
 @app.route('/delete_group', methods=['POST', 'GET'])
@@ -138,21 +113,21 @@ def todo_done():
 
         todos_done = [document_todo_done.get(str(session['group'])) for document_todo_done in cursor_todo_done]
 
-        
+
         # Find and delete the document containing the specific note
         todosdone.insert_one({str(session['group']): todo_done})
         todos.delete_one({str(session['group']): todo_done})
-        
+
     return redirect(url_for('home'))
 
 @app.route('/tododonedelete', methods=['POST'])
 def todo_done_delete():
     if request.method == 'POST':
             todo_done = request.form.get('todo_done')
-            
+
             # Find and delete the document containing the specific note
             todosdone.delete_one({str(session['group']): todo_done})
-            
+
     return redirect(url_for('home'))
 
 @app.route('/join_group', methods=['POST'])
@@ -175,7 +150,7 @@ def join_group():
         else:
             flash("That code does not match with any group!", "error")
             return redirect(url_for('home'))
-        
+
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -212,10 +187,7 @@ def create_group():
                     {"email": session['email']},
                     {"$set": {"group": custom_random_code}}
                 )
-        
-        elif user['account'] == 'basic':
-            print("a") # CODE FOR ACCEPTING PAYMENTS
-        
+
         return redirect(url_for('home'))
 
 
@@ -226,23 +198,23 @@ def signup():
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
-        
+
         if users.find_one({"email": email}):
             flash('Email already exists. Please use another email.', 'error')
             return redirect(url_for('signup'))
-        
+
         new_user = {
             "name": name,
             "email": email,
             "password": password,
-            "account": "basic",
+            "account": "leader",
             "group": "0"
         }
-        
+
         users.insert_one(new_user)
         flash('Sign-up successful! Please log in.', 'success')
         return redirect(url_for('login'))
-    
+
     return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -253,7 +225,7 @@ def login():
         if request.method == 'POST':
             email = request.form.get('email')
             password = request.form.get('password')
-            
+
             user = users.find_one({"email": email})
             if user and users.find_one({"email": email, "password":password}):
                 # User authenticated, set session variables
@@ -267,7 +239,7 @@ def login():
             else:
                 flash('Invalid email or password.', 'error')
                 return redirect(url_for('login'))
-        
+
         return render_template('login.html')
     else:
         return redirect(url_for('home'))
